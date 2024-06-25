1f700bbe3adaaa6815e24ef22326cd7b8de75beb
Roberto Romero <rpromerov@gmail.com>
2024-06-25 11:57:39 -0400

    listo

--- portRouter.go
+++ portRouter.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"context"
+	"crypto/md5"
 	"fmt"
-	"github.com/google/uuid"
 	"io"
 	"net"
 	"os"
@@ -40,7 +40,7 @@ func handleSsh(conn net.Conn) {
 	fmt.Println("Handling connection")
 	fmt.Println(conn.LocalAddr())
 	fmt.Println(conn.RemoteAddr())
-	sessionId := getSessionID()
+	sessionId := getSessionID(conn)
 
 	fmt.Println("Session ID: ", sessionId)
 
@@ -51,7 +51,7 @@ func handleSsh(conn net.Conn) {
 		fmt.Println("Rocky Linux instance created successfully.")
 		fmt.Println("IP: ", ip)
 		fmt.Println("Port: ", port)
-		rerouteSSH(conn, ip, port, sessionId)
+		rerouteConnection(conn, ip, port, sessionId)
 	}
 
 	if err != nil {
@@ -59,8 +59,8 @@ func handleSsh(conn net.Conn) {
 	}
 }
 
-func rerouteSSH(conn net.Conn, ip string, port int32, sessionId string) {
-	fmt.Println("Rerouting SSH")
+func rerouteConnection(conn net.Conn, ip string, port int32, sessionId string) {
+	fmt.Println("Rerouting connection")
 	fmt.Println("IP: ", ip)
 	address := fmt.Sprintf("%s:%d", ip, port)
 	sshConn, err := net.Dial("tcp", address)
@@ -86,18 +86,15 @@ func rerouteSSH(conn net.Conn, ip string, port int32, sessionId string) {
 		}
 		wg.Done()
 	}()
-
+	time.AfterFunc(10*time.Minute, func() {
+		removeDeploymentAndService(sessionId)
+	})
 	wg.Wait()
 	err = sshConn.Close()
 	if err != nil {
 		fmt.Println("Error: ", err)
 	}
-	removeDeploymentAndService(sessionId)
-}
 
-func getSessionID() string {
-	sessionId := uuid.New().String()
-	return sessionId
 }
 
 func requestRockyLinuxInstance(sessionId string) (string, int32, error) {
@@ -110,6 +107,11 @@ func requestRockyLinuxInstance(sessionId string) (string, int32, error) {
 	if err != nil {
 		return "", 0, err
 	}
+	ip, port, err := waitForPodAndGetAddress(clientset, sessionId, serviceName, false)
+	if ip != "" {
+		fmt.Println("Returning existing IP and port")
+		return ip, port, nil
+	}
 
 	deploymentClient := clientset.AppsV1().Deployments(corev1.NamespaceDefault)
 
@@ -121,7 +123,6 @@ func requestRockyLinuxInstance(sessionId string) (string, int32, error) {
 			Replicas: int32Ptr(1),
 			Selector: &metav1.LabelSelector{
 				MatchLabels: map[string]string{
-					"app":     "rockylinux-ssh",
 					"session": sessionId,
 				},
 			},
@@ -162,7 +163,6 @@ func requestRockyLinuxInstance(sessionId string) (string, int32, error) {
 		},
 		Spec: corev1.ServiceSpec{
 			Selector: map[string]string{
-				"app":     "rockylinux-ssh",
 				"session": sessionId,
 			},
 			Ports: []corev1.ServicePort{
@@ -181,16 +181,146 @@ func requestRockyLinuxInstance(sessionId string) (string, int32, error) {
 	}
 
 	// Wait for the pod to be running
-	ip, port, err := waitForPodAndGetAddress(clientset, sessionId, serviceName)
+	ip, port, err = waitForPodAndGetAddress(clientset, sessionId, serviceName, true)
 	if err != nil {
 		return "", 0, err
 	}
 	return ip, port, nil
 }
+func listenPostgres() {
+	fmt.Println("Listening for PostgreSQL connections on port 5432")
+	l, err := net.Listen("tcp", "0.0.0.0:5432")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error: ", err)
+			continue
+		}
+		go handlePostgres(conn)
+	}
+}
 
-func waitForPodAndGetAddress(clientset *kubernetes.Clientset, sessionId string, serviceName string) (string, int32, error) {
-	labelSelector := fmt.Sprintf("app=rockylinux-ssh,session=%s", sessionId)
+func handlePostgres(conn net.Conn) {
+	fmt.Println("Handling connection")
+	fmt.Println(conn.LocalAddr())
+	fmt.Println(conn.RemoteAddr())
+	sessionId := getSessionID(conn)
 
+	fmt.Println("Session ID: ", sessionId)
+
+	ip, port, err := requestPostgresInstance(sessionId)
+	if err != nil {
+		fmt.Println("Error creating Postgres instance:", err)
+
+	} else {
+		fmt.Println("Postgres instance created successfully.")
+		fmt.Println("IP: ", ip)
+		fmt.Println("Port: ", port)
+		rerouteConnection(conn, ip, port, sessionId)
+	}
+
+	if err != nil {
+		return
+	}
+
+}
+
+func requestPostgresInstance(sessionId string) (string, int32, error) {
+
+	serviceName := "postgres-estudiante-" + sessionId
+	config, err := getKubeConfig()
+	if err != nil {
+		return "", 0, err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return "", 0, err
+	}
+	ip, port, err := waitForPodAndGetAddress(clientset, sessionId, "postgres-estudiante-"+sessionId, false)
+	if ip != "" {
+		fmt.Println("Returning existing IP and port")
+		return ip, port, nil
+	}
+
+	deploymentClient := clientset.AppsV1().Deployments(corev1.NamespaceDefault)
+
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: serviceName,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: int32Ptr(1),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"session": sessionId,
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app":     "postgres-estudiante",
+						"session": sessionId,
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+
+							Name:            "postgres",
+							Image:           "postgres-estudiante",
+							ImagePullPolicy: corev1.PullNever,
+							Ports: []corev1.ContainerPort{
+								{
+									ContainerPort: 5432,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	_, err = deploymentClient.Create(context.TODO(), deployment, metav1.CreateOptions{})
+	if err != nil {
+		return "", 0, err
+	}
+
+	serviceClient := clientset.CoreV1().Services(corev1.NamespaceDefault)
+
+	service := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: serviceName,
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: map[string]string{
+				"session": sessionId,
+			},
+			Ports: []corev1.ServicePort{
+				{
+					Protocol:   corev1.ProtocolTCP,
+					Port:       5432,
+					TargetPort: intstr.FromInt(5432),
+				},
+			},
+			Type: corev1.ServiceTypeNodePort,
+		},
+	}
+	_, err = serviceClient.Create(context.TODO(), service, metav1.CreateOptions{})
+	if err != nil {
+		return "", 0, err
+	}
+
+	return waitForPodAndGetAddress(clientset, sessionId, serviceName, true)
+}
+
+func waitForPodAndGetAddress(clientset *kubernetes.Clientset, sessionId string, serviceName string, wait bool) (string, int32, error) {
+
+	labelSelector := fmt.Sprintf("session=%s", sessionId)
 	for {
 		podList, err := clientset.CoreV1().Pods(corev1.NamespaceDefault).List(context.TODO(), metav1.ListOptions{
 			LabelSelector: labelSelector,
@@ -228,11 +358,15 @@ func waitForPodAndGetAddress(clientset *kubernetes.Clientset, sessionId string, 
 		}
 
 		fmt.Println("Waiting for pod to be running...")
-		time.Sleep(5 * time.Second)
+		if !wait {
+			return "", 0, nil
+		}
+		time.Sleep(1 * time.Second)
 	}
 }
 
 func removeDeploymentAndService(sessionId string) {
+	fmt.Println("Removing deployment and service for session ", sessionId)
 	config, err := getKubeConfig()
 	if err != nil {
 		fmt.Println("Error: ", err)
@@ -284,10 +418,33 @@ func getKubeConfig() (*rest.Config, error) {
 	}
 	return clientcmd.BuildConfigFromFlags("", kubeconfig)
 }
+func getSessionID(conn net.Conn) string {
+	remoteIP, _, err := net.SplitHostPort(conn.RemoteAddr().String())
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return ""
+	}
 
+	_, localPort, err := net.SplitHostPort(conn.LocalAddr().String())
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return ""
+	}
+
+	combined := remoteIP + localPort
+	h := md5.New()
+	_, err = io.WriteString(h, combined)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return ""
+	}
+	hashed := fmt.Sprintf("%x", h.Sum(nil))
+	return hashed
+}
 func int32Ptr(i int32) *int32 { return &i }
 
 func main() {
 	fmt.Println("Bienvenid@!")
-	listenSsh()
+	go listenSsh()
+	listenPostgres()
 }
